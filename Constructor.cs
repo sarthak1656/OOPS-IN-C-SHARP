1cf371d6b275a645a6498a6808d1366df43df7d4
Sarthak Panigrahi <sipunp70@gmail.com>
2025-02-01 22:48:13 +0530

    Constructor

---
+++ Constructor.cs
@@ -0,0 +1,40 @@
+//Constructor is a special type method whose name is same as class name.It is invoked automatically when we create an object. The main objective of constructor is to intialize data member of new object
+
+//Types of constructor
+
+// 1.Default constructor
+
+// class A
+// {
+//     int num;
+//     A()
+//     {
+//         num=10;
+//     }
+//     public void show()
+//     {
+//         Console.Write(num);
+//     }
+//     static void Main()
+//     {
+//         A r = new A();
+//         r.show(); 
+//     }
+// }
+
+
+//2.parameter constructor
+
+class A
+{
+    
+    A(int num)
+    {
+       Console.Write(num);
+    }
+    static void Main()
+    {
+        A r = new A(50);
+         
+    }
+}
--- classObject.cs
+++ classObject.cs
@@ -2,30 +2,30 @@
 
 //Object: Objects are the instances of the class which represents the class. An object is a real entity which has some state and behaviour
 
-class mobile{
-    int price;
-    String storage;
-    String color;
-    public static void Calling(){
-        Console.WriteLine("Voice call");
-    }
-    public static void Chatting(){
-        Console.WriteLine("text call");
-    }
-    public static void Music(){
-        Console.WriteLine("song...");
-    }
-    public static void Main(string[] args){
-        mobile m = new mobile();
-        m.price= 10000;
-        m.storage="128gb";
-        m.color="red";
-        Console.WriteLine(m.price);
-        Console.WriteLine(m.storage);
-        Console.WriteLine(m.color);
-        Calling();
-        Chatting();
-        Music();
+// class mobile{
+//     int price;
+//     String storage;
+//     String color;
+//     public static void Calling(){
+//         Console.WriteLine("Voice call");
+//     }
+//     public static void Chatting(){
+//         Console.WriteLine("text call");
+//     }
+//     public static void Music(){
+//         Console.WriteLine("song...");
+//     }
+//     public static void Main(string[] args){
+//         mobile m = new mobile();
+//         m.price= 10000;
+//         m.storage="128gb";
+//         m.color="red";
+//         Console.WriteLine(m.price);
+//         Console.WriteLine(m.storage);
+//         Console.WriteLine(m.color);
+//         Calling();
+//         Chatting();
+//         Music();
 
-    }
-}
+//     }
+// }
